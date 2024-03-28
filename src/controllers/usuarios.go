9b82ff8c90c3fc06f8f26e9d7e44ef12f7540b69
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-03-28 11:05:00 -0300

    Feature: Added user repository

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,10 +1,34 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	corpoRequest, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var usuario modelos.Usuario
+	if err = json.Unmarshal(corpoRequest, &usuario); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := banco.Conectar()
+	if err != nil {
+		log.Fatal(err)
+	}
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	repositorio.Criar(usuario)
+
 }
+
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Busca todos usuários"))
 }
