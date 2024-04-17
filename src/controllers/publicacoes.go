87e695dd00c3c6261f18bd6e7c3f522ca168ef06
dpcamargo <dpcamargo@gmail.com>
2024-04-16 23:20:27 -0300

    Feature: Create publication

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -1,8 +1,53 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+)
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, err := autenticacao.ExtrairUsuarioID(r)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	corpoRequisicao, err := io.ReadAll(r.Body)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if err := json.Unmarshal(corpoRequisicao, &publicacao); err != nil {
+		respostas.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, err := banco.Conectar()
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, err = repositorio.Criar(publicacao)
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 
 }
 
